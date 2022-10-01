34865026820abe87bfcbf5231a1b665e2c614a37
Deepak Verma <deepak.v@ayu.health>
2022-10-01 16:52:53 +0530

    added UI

--- f/application/config.php
+++ f/application/config.php
@@ -3,5 +3,5 @@ $db_host	    = 'localhost';
 $db_user        = 'root';
 $db_password	= '';
 $db_name	    = 'cargo';
-define('APP_URL', 'http://localhost/firstvisioncargo/f/');
+define('APP_URL', '/firstvisioncargo/f/');
 $_app_stage = 'Live'; // You can set this variable Live to Dev to enable ibilling Debug
